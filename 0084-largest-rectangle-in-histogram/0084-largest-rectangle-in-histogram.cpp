c84d4551a3c6998498d35c6b331217f0e3ac8970
Naman sharma <sharma.naman.in@gmail.com>
2026-07-07 18:45:23 +0530

    Time: 23 ms (54.14%), Space: 79.4 MB (96.5%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
-    vector<int>  findRightSmaller(vector<int>&heights,int n)
-    {
-       vector<int>ans(n);
-       stack<int>s;
-
-       for(int i=n-1; i>=0; i--)
-       {
-           while(!s.empty() && heights[s.top()]>=heights[i])
-           {
-             s.pop();
-           }
-
-           ans[i]=s.empty() ? n : s.top();
-
-           s.push(i);
-       }
-      return ans;
-    }
-    vector<int>  findLeftSmaller(vector<int>&heights,int n)
-    {
-       vector<int>ans(n);
-       stack<int>s;
-
-       for(int i=0; i<n; i++)
-       {
-           while(!s.empty() && heights[s.top()]>=heights[i])
-           {
-             s.pop();
-           }
-
-           ans[i]=s.empty() ? -1 : s.top();
-
-           s.push(i);
-       }
-      return ans;
-    }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int>left=findLeftSmaller(heights,n);
-        vector<int>right=findRightSmaller(heights,n);
+        stack<int>s;
         int ans=INT_MIN;
 
-         for(int i=0; i<n; i++)
-         {
-            int h=heights[i];
-            int w=right[i]-left[i]-1;
-            ans=max(ans,h*w);
-         }
-         return ans;
+        for(int i=0; i<=n; i++)
+        {
+            int currEle=(i==n) ? 0 : heights[i];
+            while(!s.empty() && heights[s.top()]>=currEle)
+            {
+                int idx=s.top();
+                s.pop();
+                int h=heights[idx];
+                int prev=(s.empty()) ? -1 : s.top();
+                int w=i-prev-1;
+                ans=max(ans,h*w);
+            }
+
+            s.push(i);
+        }
+        return ans;
     }
 };
