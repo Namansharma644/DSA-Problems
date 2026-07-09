3011ec8e1e55c86745dbeb843178cef32880e20b
Naman sharma <sharma.naman.in@gmail.com>
2026-07-09 13:05:53 +0530

    Time: 3 ms (28.35%), Space: 21.4 MB (97.33%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>ans;
+        for(int &a : asteroids)
+        {
+            while(!ans.empty() && ans.back()>0 && a<0)
+            {
+                int sum=ans.back()+a;
+                if(sum<0)
+                {
+                    ans.pop_back();
+                }
+                else if(sum>0)
+                {
+                    a=0;
+                }
+                else
+                {
+                    ans.pop_back();
+                    a=0;
+                }
+            }
+
+            if(a!=0){
+                ans.push_back(a);
+            }
+        }
+     return ans;
+    }
+};
