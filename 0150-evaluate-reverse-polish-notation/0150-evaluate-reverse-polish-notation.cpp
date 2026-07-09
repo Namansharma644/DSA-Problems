470552e55c6cabbaa5b2f38f978244ca81765ec5
Naman sharma <sharma.naman.in@gmail.com>
2026-07-09 19:03:37 +0530

    Time: 3 ms (36.27%), Space: 17.4 MB (10.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int operate(int a,int b,string &token)
-    {
-        if(token=="+")
-        {
-            return b+a;
-        }
-        if(token=="-")
-        {
-            return b-a;
-        }
-        if(token=="*")
-        {
-            return (long)b*(long)a;
-        }
-        if(token=="/")
-        {
-            return b/a;
-        }
-        return -1;
-    }
     int evalRPN(vector<string>& tokens) {
         stack<int>s;
+
+        unordered_map<string , function<int(int,int)>>m={
+              {"+",[](int a,int b){ return b+a;} },
+              {"-",[](int a,int b){return b-a;} },
+              {"/",[](int a,int b){return b/a;} },
+              {"*",[](int a,int b){return (long long)b*(long long)a;}}
+        };
+
         for(string &token : tokens)
         {
             if(token=="+" || token=="-" || token=="*" || token=="/")
@@ -32,7 +20,7 @@ public:
                  int b=s.top();
                  s.pop();
 
-                 int res=operate(a,b,token);
+                 int res=m[token](a,b);
                  s.push(res);
             }
             else
