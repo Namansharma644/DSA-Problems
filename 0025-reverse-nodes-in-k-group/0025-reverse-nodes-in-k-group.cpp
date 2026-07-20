cbde9f254d065cb941a319d1ed29337a4af13b6a
Naman sharma <sharma.naman.in@gmail.com>
2026-07-21 02:00:38 +0530

    Time: 0 ms (100%), Space: 16.7 MB (6.41%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,30 +10,54 @@
  */
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* curr=head;
-        for(int i=0; i<k; i++)
+    int len(ListNode* head)
+    {
+        int count=0;
+        ListNode* temp=head;
+
+        while(temp!=NULL)
         {
-            if(curr==NULL)
+            count++;
+            temp=temp->next;
+        }
+        return count;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int n=len(head);
+        int groups=n/k;
+        ListNode* prevHead=NULL;
+        ListNode* currHead=head;
+        ListNode* ansNode=NULL;
+
+        for(int i=0; i<groups; i++)
+        {
+            ListNode* prev=NULL;
+            ListNode* curr=currHead;
+            ListNode* nextNode=NULL;
+            int count=0;
+
+            while(count<k)
             {
-                return head;
+                nextNode=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=nextNode;
+                count++;
             }
-            curr=curr->next;
+
+            if(prevHead==NULL)
+            {
+                ansNode=prev;
+            }
+            else
+            {
+               prevHead->next=prev;
+            }
+            prevHead=currHead;
+            currHead=curr;
         }
 
-        ListNode* prev=NULL;
-        ListNode* nextNode=NULL;
-        curr=head;
-        int count=0;
-        while(count<k)
-        {
-           nextNode=curr->next;
-           curr->next=prev;
-           prev=curr;
-           curr=nextNode;
-           count++;
-        }
-        head->next=reverseKGroup(curr,k);
-        return prev;
+        prevHead->next=currHead;
+        return ansNode;
     }
 };
