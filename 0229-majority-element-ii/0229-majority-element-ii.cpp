3a74586d6fd6302b351b0d86a1afa5d0d234f5ee
Naman sharma <sharma.naman.in@gmail.com>
2026-07-31 23:22:38 +0530

    Time: 0 ms (100%), Space: 23.6 MB (68.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,20 +3,56 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans;
-        unordered_map<int,int>freq;
+        int count1=0;
+        int count2=0;
+        int maj1=0;
+        int maj2=1;
 
         for(int i=0; i<n; i++)
         {
-            freq[nums[i]]++;
-        }
-        
-        for(auto &[key,value] : freq)
-        {
-            if(value>n/3)
+            if(maj1==nums[i])
             {
-                ans.push_back(key);
+                count1++;
+            }
+            else if(maj2==nums[i])
+            {
+                count2++;
+            }
+            else if(count1==0)
+            {
+                maj1=nums[i];
+                count1=1;
+            }
+            else if(count2==0)
+            {
+                maj2=nums[i];
+                count2=1;
+            }
+            else
+            {
+                count1--;
+                count2--;
             }
         }
+
+        int freq1=0;
+        int freq2=0;
+        for(int &num : nums)
+        {
+            if(num==maj1) freq1++;
+            if(num==maj2) freq2++;
+        }
+
+        if(freq1>n/3)
+        {
+            ans.push_back(maj1);
+        }
+
+        if(freq2>n/3)
+        {
+            ans.push_back(maj2);
+        }
+
         return ans;
     }
 };
