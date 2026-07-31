2eaaf6cb2d4441af40b0825d6de1cf1836a75fad
Naman sharma <sharma.naman.in@gmail.com>
2026-07-31 22:23:20 +0530

    Time: 0 ms (100%), Space: 53.1 MB (76.84%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -3,20 +3,21 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans;
-        unordered_set<int>s;
 
         for(int i=0; i<n; i++)
         {
-            s.insert(nums[i]);
+            int idx=abs(nums[i])-1;
+            nums[idx]=-abs(nums[idx]);
         }
 
-        for(int i=1; i<=n; i++)
+        for(int i=0; i<n; i++)
         {
-            if(s.find(i)==s.end())
+            if(nums[i]>0)
             {
-                ans.push_back(i);
+                ans.push_back(i+1);
             }
         }
+
         return ans;
     }
 };
