db9358a44a4e7bb286d12015e4ad30ddec470252
Naman sharma <sharma.naman.in@gmail.com>
2026-08-06 22:45:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        if(s.size()!=t.size())
+        {
+            return  false;
+        }
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+
+        for(int i=0; i<n; i++)
+        {
+            freq1[s[i]-'a']++;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            freq2[t[i]-'a']++;
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            if(freq1[i]!=freq2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
