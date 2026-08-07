e4ad26f8f5a1b6acf03731589d4f125a1bf927cd
Naman sharma <sharma.naman.in@gmail.com>
2026-08-07 23:15:07 +0530

    Time: 26 ms (23.28%), Space: 29.6 MB (18.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    string genrateWord(string &word)
+    {
+        vector<int>freq(26,0);
+        for(int i=0; i<word.size(); i++)
+        {
+            freq[word[i]-'a']++;
+        }
+        string s="";
+        for(int i=0; i<26; i++)
+        {
+             if(freq[i]>0)
+             {
+                s+=string(freq[i],i+'a');
+             }
+        }
+        return s;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n=strs.size();
         unordered_map<string,vector<string>>map;
@@ -8,8 +25,8 @@ public:
         for(int i=0; i<n; i++)
         {
             string temp=strs[i];
-            sort(temp.begin(),temp.end());
-            map[temp].push_back(strs[i]);
+            string new_temp=genrateWord(temp);
+            map[new_temp].push_back(strs[i]);
         }
 
         for(auto &it : map)
