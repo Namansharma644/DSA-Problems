ed531cb77008b998e7886afc1331d2ecf7f18776
Naman sharma <sharma.naman.in@gmail.com>
2026-08-16 22:11:29 +0530

    Time: 19 ms (39.1%), Space: 10.6 MB (31.58%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int balancedString(string s) {
+        int n = s.size();
+        int required = n / 4;
+
+        unordered_map<char, int> mp;
+
+        for (char c : s)
+            mp[c]++;
+
+        if (mp['Q'] == required &&
+        mp['W'] == required &&
+        mp['E'] == required &&
+        mp['R'] == required)
+            return 0;
+
+        int i = 0;
+        int ans = n;
+
+        for (int j = 0; j < n; j++) {
+
+            mp[s[j]]--;
+
+            while (i<=j && mp['Q'] <= required &&
+                   mp['W'] <= required &&
+                   mp['E'] <= required &&
+                   mp['R'] <= required) {
+
+                ans = min(ans, j - i + 1);
+
+                mp[s[i]]++;
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
