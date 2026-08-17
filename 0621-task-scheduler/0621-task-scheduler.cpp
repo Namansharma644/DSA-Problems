7d7350a7610ebb027da1ac74048da72fa837927d
Naman sharma <sharma.naman.in@gmail.com>
2026-08-18 00:18:41 +0530

    Time: 0 ms (100%), Space: 38.2 MB (84.08%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -4,54 +4,27 @@ public:
         int k=tasks.size();
         vector<int>map(26,0);
 
-        for(char &c : tasks){
+        for(char &c : tasks)
+        {
             map[c-'A']++;
         }
 
-        int times=0;
-        priority_queue<int>pq;
+        sort(map.begin(),map.end());
 
-        for(int i=0; i<26; i++)
+        int maxFreq=map[25];
+        int slots=maxFreq-1;
+        int isolatedSlots=slots*n;
+
+        for(int i=24; i>=0; i--)
         {
-            if(map[i]>0)
-            {
-                pq.push(map[i]);
-            }
+            isolatedSlots-=min(map[i],slots);
         }
 
-        while(!pq.empty())
+        if(isolatedSlots<0)
         {
-            vector<int>temp;
-
-            for(int i=1; i<=n+1; i++)
-            {
-                if(!pq.empty())
-                {
-                    int freq=pq.top();
-                    pq.pop();
-                    freq--;
-                    temp.push_back(freq);
-                }
-            }
-
-            for(int &f  : temp)
-            {
-                if(f>0)
-                {
-                    pq.push(f);
-                }
-            }
-
-            if(pq.empty())
-            {
-                times+=temp.size();
-            }
-            else
-            {
-                times+=(n+1);
-            }
+            return tasks.size();
         }
 
-        return times;
+        return tasks.size()+isolatedSlots;
     }
 };
