9c7391764bba344a3e2f54ae3a4a26d13f1af636
Naman sharma <sharma.naman.in@gmail.com>
2026-09-04 15:22:47 +0530

    Time: 0 ms (100%), Space: 18.5 MB (14.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color,vector<vector<int>>&directions,int intialColor)
+    {
+        image[sr][sc]=color;
         int n=image.size();
         int m=image[0].size();
+
+        for(auto &dir : directions)
+        {
+            int i=sr+dir[0];
+            int j=sc+dir[1];
+
+            if(i>=0 && i<n && j>=0 && j<m && image[i][j]==intialColor)
+            {
+                dfs(image,i,j,color,directions,intialColor);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+      
 
         int intialColor=image[sr][sc];
         if(intialColor==color) return image;
@@ -11,24 +27,8 @@ public:
         q.push({sr,sc});
         image[sr][sc]=color;
         vector<vector<int>>directions={{0,1},{1,0},{0,-1},{-1,0}};
+        dfs(image,sr,sc,color,directions,intialColor);
 
-        while(!q.empty())
-        {
-            auto [r,c]=q.front();
-            q.pop();
-
-            for(auto &dir : directions)
-            {
-                int i=r+dir[0];
-                int j=c+dir[1];
-
-                if(i>=0 && i<n && j>=0 && j<m && image[i][j]==intialColor)
-                {
-                    image[i][j]=color;
-                    q.push({i,j});
-                }
-            }
-        } 
         return image;
     }
 };
