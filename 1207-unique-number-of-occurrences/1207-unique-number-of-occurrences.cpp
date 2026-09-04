77d9555217a6cc2c66a94a939ad2d0cdff749e9a
Naman sharma <sharma.naman.in@gmail.com>
2026-09-04 15:45:56 +0530

    Time: 3 ms (14.1%), Space: 11.9 MB (71.64%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>m;
+
+        for(int &x : arr)
+        {
+            m[x]++;
+        }
+
+        unordered_set<int>s;
+
+        for(auto &x : m)
+        {
+            s.insert(x.second);
+        }
+
+        return m.size()==s.size();
+    }
+};
